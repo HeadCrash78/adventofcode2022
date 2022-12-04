47ec2f08d34c8e2aea28ee2f79c0d07028b91ad7
René Schumacher <rene.schumacher@microsoft.com>
2022-12-04 23:38:22 +0100

    Remove unnecessary async

--- day1-part2.ts
+++ day1-part2.ts
@@ -1,27 +1,23 @@
 import * as fs from 'fs';
 
-async function run() {
-    const inputData = fs.readFileSync('day1-input.txt', { encoding: 'utf-8' });
-    let elves: {key: number, calories: number}[] = [];
-    let elveIndex = 1;
-    let totalCalories = 0;
-    if (inputData.length > 0) {
-        let calories = 0;
-        inputData.split('\n').forEach((l: string) => {
-            if (l.length == 0) {
-                elves.push({key: elveIndex++, calories: calories});
-                calories = 0;
-            } else {
-                calories += Number(l);
-                totalCalories += Number(l);
-            }
-        });
-    }
-    let topThreeElves = elves.sort((a,b) => b.calories - a.calories).slice(0,3);
-    let totalTopThreeCalories = 0;
-    topThreeElves.forEach(e => { totalTopThreeCalories += e.calories });
-    console.log(`There are ${elves.length} carrying ${totalCalories} total calories.`);
-    console.log(`Elves no. ${topThreeElves.map(e => e.key).join(', ')} with ${totalTopThreeCalories} carry the most.`)
+const inputData = fs.readFileSync('day1-input.txt', { encoding: 'utf-8' });
+let elves: {key: number, calories: number}[] = [];
+let elveIndex = 1;
+let totalCalories = 0;
+if (inputData.length > 0) {
+    let calories = 0;
+    inputData.split('\n').forEach((l: string) => {
+        if (l.length == 0) {
+            elves.push({key: elveIndex++, calories: calories});
+            calories = 0;
+        } else {
+            calories += Number(l);
+            totalCalories += Number(l);
+        }
+    });
 }
-
-run();
+let topThreeElves = elves.sort((a,b) => b.calories - a.calories).slice(0,3);
+let totalTopThreeCalories = 0;
+topThreeElves.forEach(e => { totalTopThreeCalories += e.calories });
+console.log(`There are ${elves.length} carrying ${totalCalories} total calories.`);
+console.log(`Elves no. ${topThreeElves.map(e => e.key).join(', ')} with ${totalTopThreeCalories} carry the most.`);
