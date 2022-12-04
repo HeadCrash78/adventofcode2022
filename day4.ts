49a0332561c6cfea1b232bae787a7bbcb435539f
René Schumacher <rene.schumacher@microsoft.com>
2022-12-05 00:42:38 +0100

    Add day4 code

---
+++ day4.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+
+const inputData = fs.readFileSync('day4-input.txt', { encoding: 'utf-8' });
+let fullyContained = 0;
+let overlap = 0;
+inputData.split('\n').forEach(line => {
+    let tokens = line.split(/[-,]/);
+    let e1Start = Number(tokens[0]);
+    let e1End = Number(tokens[1]);
+    let e2Start = Number(tokens[2]);
+    let e2End = Number(tokens[3]);
+    fullyContained += e1Start == e2Start || e1End == e2End
+        || e1Start < e2Start && e2End < e1End
+        || e2Start < e1Start && e1End < e2End ? 1 : 0;
+    overlap += Math.max(e1Start, e2Start) <= Math.min(e1End, e2End) ? 1 : 0;
+});
+console.log(`There are ${fullyContained} fully contained segments`);
+console.log(`There are ${overlap} overlapping segments overlap`);
