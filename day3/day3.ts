e5bc801b878b5ce6b337a3eb91445dde93367d48
René Schumacher <rene.schumacher@microsoft.com>
2022-12-06 13:08:03 +0100

    Slight improvement of day 3 code, fix Git in dev container

--- day3/day3.ts
+++ day3/day3.ts
@@ -1,5 +1,10 @@
 import { getInputData } from '../common/helpers';
 
+function getItemPrio(item: string): number {
+    let charCode = item.charCodeAt(0);
+    return charCode >= 97 ? charCode - 96 : charCode - 38;
+}
+
 const inputData = getInputData();
 let sumOfPrios = 0;
 let sumOfBadgePrios = 0;
@@ -8,12 +13,9 @@ inputData.forEach(line => {
     group.push(line);
     let firstComp = line.substring(0,line.length / 2);
     let secondCompRegex = new RegExp(`[${line.substring(line.length / 2)}]`);
-    let itemInBothCompsPrio = firstComp.match(secondCompRegex)![0].charCodeAt(0);
-    sumOfPrios += itemInBothCompsPrio >= 97 ? itemInBothCompsPrio - 96 : itemInBothCompsPrio - 38;
+    sumOfPrios += getItemPrio(firstComp.match(secondCompRegex)![0]);
     if (group.length % 3 == 0) {
-        let commonItemPrio = group[0].match(new RegExp(`[${group[1]}]`, 'g'))!.join('')
-            .match(new RegExp(`[${group[2]}]`))![0].charCodeAt(0);
-        sumOfBadgePrios += commonItemPrio >= 97 ? commonItemPrio - 96 : commonItemPrio - 38;
+        sumOfBadgePrios += getItemPrio(group[0].match(new RegExp(`[${group[1]}]`, 'g'))!.join('').match(new RegExp(`[${group[2]}]`))![0]);
         group = [];
     }
 });
