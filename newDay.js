09b45dc593c2b9f646405ecc6bbfb67ce9b9c285
René Schumacher <rene.schumacher@microsoft.com>
2022-12-11 09:28:24 +0100

    Fix new day script

--- newDay.js
+++ newDay.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 let day = process.argv[2];
-fs.mkdir(day, (e, p) => {});
+fs.mkdirSync(day, (e, p) => {});
 fs.writeFileSync(path.join(day, `${day}.ts`), '');
 fs.writeFileSync(path.join(day, 'input.txt'), '');
