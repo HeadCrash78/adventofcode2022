7f8ebbe5bd18aa9fb27cf9a950f8f2309143cdf8
René Schumacher <rene.schumacher@microsoft.com>
2022-12-15 00:39:14 +0100

    Add some puzzle descriptions

--- newDay.js
+++ newDay.js
@@ -5,3 +5,4 @@ let day = process.argv[2];
 fs.mkdirSync(day, (e, p) => {});
 fs.writeFileSync(path.join(day, `${day}.ts`), '');
 fs.writeFileSync(path.join(day, 'input.txt'), '');
+fs.writeFileSync(path.join(day, 'puzzle.md'), '');
